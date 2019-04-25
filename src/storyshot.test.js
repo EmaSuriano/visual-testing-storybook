ff520d35679fd272d9636567b183d1c14b9bc750
EmaSuriano <emanuel.suriano@gmail.com>
2019-04-25 23:34:34 +0200

    Adding visual testing

--- src/storyshot.test.js
+++ src/storyshot.test.js
@@ -1,5 +1,6 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
 initStoryshots({
-  /* configuration options */
+  test: imageSnapshot({ storybookUrl: 'http://localhost:9009/' }),
 });
